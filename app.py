0387391119bbe07510a0ec14ab52d64d7edd9a71
Rapuru Varshini <rapurvarshini2964@gmail.com>
2026-08-28 09:34:46 +0530

    Updated ExamGuard files

--- app.py
+++ app.py
@@ -1,38 +1,30 @@
-from flask import Flask, request, render_template
-from database import init_db , get_db
+from flask import Flask, redirect, request, render_template, session
+from database import init_db, get_db
 
 app = Flask(__name__)
+app.secret_key = "examguard_secret_key"
+
+print(app.url_map)
+
 init_db()
+
 
 @app.route("/")
 def home():
     return "Welcome to Exam Guard"
 
 
-# @app.route("/register", methods=["GET", "POST"])
-# def register():
-
-#     if request.method == "POST":
-#         name = request.form["name"]
-#         email = request.form["email"]
-#         password = request.form["password"]
-
-#         print("name:", name)
-#         print("email:", email)
-#         print("password:", password)
-
-#     return render_template("register.html")
-
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
 
     if request.method == "POST":
+
         name = request.form["name"]
         email = request.form["email"]
         password = request.form["password"]
 
         connection = get_db()
+
         connection.execute(
             """
             INSERT INTO candidates
@@ -41,17 +33,14 @@ def register():
             """,
             (name, email, password)
         )
-        
+
         connection.commit()
         connection.close()
-        
-        return "registration successful"
 
-        print("name:", name)
-        print("email:", email)
-        print("password:", password)
+        return "Registration successful"
 
     return render_template("register.html")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -75,12 +64,32 @@ def login():
         connection.close()
 
         if candidate:
-            return "Login successful!"
+            session["candidate_id"] = candidate["id"]
+            return redirect("/dashboard")
 
         return "Invalid email or password"
 
     return render_template("login.html")
 
-if  __name__=="__main__":
-    init_db()
+
+@app.route("/dashboard")
+def dashboard():
+
+    if "candidate_id" not in session:
+        return "Please login first"
+
+    return render_template("dashboard.html")
+
+
+@app.route("/logout")
+def logout():
+
+    session.clear()
+    return redirect("/login")
+
+
+
+
+if __name__ == "__main__":
+    print(app.url_map)
     app.run(debug=True)
